da161ead2b5de20aaf17b9f78e058e6a00313712
Abdul Rub <abdulrub9542@gmail.com>
2021-01-02 18:34:46 +0530

    saturday work

--- myservice/src/Components/Albums.js
+++ myservice/src/Components/Albums.js
@@ -108,12 +108,7 @@ export class Albums extends Component {
                                                             <label for="name" class="leading-7 text-sm text-gray-600">Title</label>
                                                             <input type="text" id="name" name="name" value={this.state.title} onChange={(event)=>this.setState({title:event.target.value})} class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
                                                         </div>
-                                                        <div class="p-2 w-full">
-                                                            <div class="relative">
-                                                              <label for="message" class="leading-7 text-sm text-gray-600">Body</label>
-                                                              <textarea id="message" name="message" value={this.state.body} onChange={(event)=>this.setState({body:event.target.value})} class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-                                                            </div>
-                                                        </div>
+                                                        
                                                         <div class="p-2 w-full">
                                                             <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" onClick={()=>this.update(index)}>Save</button>
                                                         </div>
